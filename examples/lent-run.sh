759bcbc2199a4ff864e0e000b50a566cf6bdb77f
Julio-Anjos <julio.c.s.anjos@gmail.com>
2018-03-30 10:42:49 -0300

    Version Adjusts

--- examples/lent-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$HOME/simgrid-3.11.1/lib
-
-./hello_mrsg.bin --cfg=surf/precision:0.0000000000001 2>&1 | $HOME/simgrid-3.11.1/bin/simgrid-colorizer
-#./hello_mrsg.bin --cfg=maxmin/precision:0.0000000001 2>&1 | $HOME/simgrid-3.11.1/bin/simgrid-colorizer
-
